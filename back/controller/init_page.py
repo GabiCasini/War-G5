da17e0568d50a3bc15f81e5f7d537a0199d8cb70
GabiCasini <gabizinhacarpenter@gmail.com>
2025-09-17 16:41:03 -0300

    redirect para mapa ao iniciar partida

--- back/controller/init_page.py
+++ back/controller/init_page.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, request
+from flask import Blueprint, redirect, render_template, request
 
 from model.Partida import Partida
 
@@ -9,9 +9,12 @@ class PaginaIndex:
     @init_page_bp.route('/inicializar_partida', methods=['POST'])
     def inicializar_partida():
         if request.method == 'POST':
-            qtd_humanos = int(request.form.get('qtd_humanos'))
-            qtd_ai = int(request.form.get('qtd_ai'))
-            duracao_turno = request.form.get('duracao_turno')
-            partida = Partida(qtd_humanos, qtd_ai, duracao_turno)
-
-            return redirect('/') 
+            if request.form.get('acao') == 'iniciar':
+                qtd_humanos = int(request.form.get('qtd_humanos'))
+                qtd_ai = int(request.form.get('qtd_ai'))
+                duracao_turno = request.form.get('duracao_turno')
+                partida = Partida(qtd_humanos, qtd_ai, duracao_turno)
+                
+                return render_template('mapa.html', partida=partida)
+            
+            return redirect('/')
