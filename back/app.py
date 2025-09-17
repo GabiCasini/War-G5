1a8296aa5f054b04df7ffab567266f15993720e6
GabiCasini <gabizinhacarpenter@gmail.com>
2025-09-17 15:11:11 -0300

    add controller para pag inicial

--- back/app.py
+++ back/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from controller.init_page import init_page_bp
 
 app = Flask(__name__)
+app.register_blueprint(init_page_bp)
 
 @app.route('/')
 def index():
---
+++ back/controller/init_page.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, redirect, request
+
+from model.Partida import Partida
+
+init_page_bp = Blueprint('init_page', __name__)
+
+class PaginaIndex:
+
+    @init_page_bp.route('/inicializar_partida', methods=['POST'])
+    def submit_form():
+        if request.method == 'POST':
+            qtd_humanos = int(request.form.get('qtd_humanos'))
+            qtd_ai = int(request.form.get('qtd_ai'))
+            duracao_turno = request.form.get('duracao_turno')
+            partida = Partida(qtd_humanos, qtd_ai, duracao_turno)
+
+            return redirect('/') 
