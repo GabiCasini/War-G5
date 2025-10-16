bfd91e9c9c4b9e802c17af28afbb17400aa87011
Rafael <139813923+Rafael-Machado-27@users.noreply.github.com>
2025-10-16 17:17:02 -0300

    Atualização da lista completa de territórios no back

--- back/model/Tabuleiro.py
+++ back/model/Tabuleiro.py
@@ -4,13 +4,54 @@ from .Territorio import Territorio
 from .Jogador import Jogador
 
 # lista onde cada elemento é uma lista que possui informações de cada territorio do jogo -> [nome, regiao, lista de fronteiras]
-TERRITORIOS = [["Rio de Janeiro", "Regiao1", ["Niterói", "Mesquita"]], ["Niterói", "Regiao2", ["Rio de Janeiro", "Maricá"]],
-               ["Mesquita", "Regiao1", ["Rio de Janeiro", "Maricá"]], ["Maricá", "Regiao2", ["Niterói", "Mesquita"]]]
+TERRITORIOS = [["Rio de Janeiro", "Regiao_1", ["Nova Iguaçu", "Mesquita", "São João de Meriti", "Niterói"]],
+               ["Nova Iguaçu", "Regiao_1", ["Rio de Janeiro", "Mesquita", "Seropédica"]],
+               ["Mesquita", "Regiao_1", ["Rio de Janeiro", "Nova Iguaçu", "São João de Meriti"]],
+               ["São João de Meriti", "Regiao_1", ["Rio de Janeiro", "Mesquita"]], 
+               ["Seropédica", "Regiao_2", ["Nova Iguaçu", "Queimados", "Japeri", "Paracambi"]],
+               ["Queimados", "Regiao_2", ["Japeri", "Seropédica"]],
+               ["Japeri", "Regiao_2", ["Miguel Pereira", "Queimados", "Seropédica", "Paracambi"]],
+               ["Paracambi", "Regiao_2", ["Miguel Pereira", "Eng Paulo de Frontin", "Seropédica", "Japeri"]],
+               ["Miguel Pereira", "Regiao_2", ["Paty do Alferes", "Vassouras", "Eng Paulo de Frontin", "Japeri", "Paracambi"]],
+               ["Eng Paulo de Frontin", "Regiao_2", ["Vassouras", "Miguel Pereira", "Paracambi"]],
+               ["Vassouras", "Regiao_2", ["Paty do Alferes", "Miguel Pereira", "Eng Paulo de Frontin", "Paraíba do Sul"]],
+               ["Paty do Alferes", "Regiao_2", ["Vassouras", "Miguel Pereira", "Paraíba do Sul"]],
+               ["Paraíba do Sul", "Regiao_2", ["Paty do Alferes", "Vassouras", "Comendador Levy Gasparian"]],
+               ["Comendador Levy Gasparian", "Regiao_3", ["Três Rios", "Paraíba do Sul"]],
+               ["Três Rios", "Regiao_3", ["Comendador Levy Gasparian", "Sapucaia", "Areal"]],
+               ["Areal", "Regiao_3", ["Petrópolis", "Três Rios"]],
+               ["Sapucaia", "Regiao_3", ["Três Rios", "Teresópolis", "São José do Vale do Rio Preto"]],
+               ["Petrópolis", "Regiao_3", ["Areal", "Teresópolis", "Magé", "Guapimirim"]],
+               ["Teresópolis", "Regiao_3", ["Sapucaia", "Petrópolis", "Cachoeiras de Macacu", "Guapimirim", "Nova Friburgo"]],
+               ["Cachoeiras de Macacu", "Regiao_3", ["Teresópolis", "Guapimirim", "Itaboraí"]],
+               ["São José do Vale do Rio Preto", "Regiao_4", ["Sapucaia", "Sumidouro"]],
+               ["Sumidouro", "Regiao_4", ["São José do Vale do Rio Preto", "Carmo", "Duas Barras", "Nova Friburgo"]],
+               ["Nova Friburgo", "Regiao_4", ["Teresópolis", "Sumidouro", "Duas Barras", "Bom Jardim", "Cordeiro", "Trajano de Moraes"]],
+               ["Bom Jardim", "Regiao_4", ["Duas Barras", "Nova Friburgo", "Macuco"]],
+               ["Duas Barras", "Regiao_4", ["Cantagalo", "Carmo", "Sumidouro", "Nova Friburgo", "Bom Jardim"]],
+               ["Carmo", "Regiao_4", ["Cantagalo", "Duas Barras", "Sumidouro"]],
+               ["Cantagalo", "Regiao_4", ["Duas Barras", "Carmo", "Macuco", "São Sebastião do Alto", "Itaocara", "Santo Antônio de Pádua"]],
+               ["Macuco", "Regiao_4", ["Cantagalo", "São Sebastião do Alto", "Bom Jardim"]],
+               ["São Sebastião do Alto", "Regiao_4", ["Cantagalo", "Macuco", "Itaocara"]],
+               ["Itaocara", "Regiao_4", ["Cantagalo", "São Sebastião do Alto", "Santo Antônio de Pádua", "Cambuci"]],
+               ["Santo Antônio de Pádua", "Regiao_4", ["Cantagalo", "Itaocara", "Cambuci"]],
+               ["Cambuci", "Regiao_4", ["Santo Antônio de Pádua", "Itaocara"]],
+               ["Magé", "Regiao_5", ["Petrópolis", "Guapimirim"]],
+               ["Guapimirim", "Regiao_5", ["Petrópolis", "Magé", "Teresópolis", "Cachoeiras de Macacu", "Itaboraí"]],
+               ["Itaboraí", "Regiao_5", ["Guapimirim", "Cachoeiras de Macacu", "São Gonçalo", "Maricá"]],
+               ["São Gonçalo", "Regiao_5", ["Itaboraí", "Maricá", "Niterói"]],
+               ["Maricá", "Regiao_5", ["Itaboraí", "São Gonçalo", "Niterói"]],
+               ["Niterói", "Regiao_5", ["São Gonçalo", "Maricá", "Rio de Janeiro"]],
+               ["Cordeiro", "Regiao_6", ["Nova Friburgo", "Trajano de Moraes"]],
+               ["Trajano de Moraes", "Regiao_6", ["Nova Friburgo", "Cordeiro", "Macaé"]],
+               ["Macaé", "Regiao_6", ["Trajano de Moraes", "Casimiro de Abreu"]],
+               ["Casimiro de Abreu", "Regiao_6", ["Macaé"]]
+               ]
 
 class Tabuleiro:
     def __init__(self, jogadores):
         self.territorios = self.gerar_territorios(len(jogadores), jogadores)
-        self.regioes_com_bonus = [["Regiao1", 2], ["Regiao2", 3]]
+        self.regioes_com_bonus = [["Regiao_1", 2], ["Regiao_2", 3]]
         
     #inicializa os territorios, atribui suas fronteiras, e atribui cada territorio a um jogador de acordo com a sua cor
     def gerar_territorios(self, num, jogadores):
--- back/model/teste_tabuleiro.py
+++ back/model/teste_tabuleiro.py
@@ -1,28 +1,17 @@
 from .Partida import Partida
 
-partida = Partida(3, 0, 120, [["Jogador1", "Vermelha"], ["Jogador2", "Azul"]])
+partida = Partida(3, 0, 120, [["Alice", "vermelho", "humano"], ["Bob", "azul", "humano"], ["Charlie", "verde", "humano"]])
 print("Ordem de turno:")
 print(partida.jogadores)
 print()
 
-for i in partida.tabuleiro.territorios:
-    print(i)
-    print(i.fronteiras)
-    print()
-
-#partida.jogadores[0].territorios = partida.tabuleiro.recuperar_territorios_por_cor(partida.jogadores[0].cor)
-print("Territorios do Jogador Vermelho:")
-print(partida.jogadores[0].territorios)
-
-#partida.jogadores[1].territorios = partida.tabuleiro.recuperar_territorios_por_cor(partida.jogadores[1].cor)
-print("\nTerritorios do Jogador Azul:")
-print(partida.jogadores[1].territorios)
-
 partida.jogadores[0].territorios[0].exercitos = 3
-partida.tabuleiro.territorios[1].exercitos = 5
+partida.tabuleiro.territorios[5].exercitos = 5
 
-print("\nAlterando exercitos:")
-for i in partida.tabuleiro.territorios:
-    print(i)
-    print(i.fronteiras)
+for i in partida.jogadores:
+    print(f"Territórios do Jogador {i.cor}")
     print()
+    for j in i.territorios:
+        print(j)
+        print(j.fronteiras)
+        print()
