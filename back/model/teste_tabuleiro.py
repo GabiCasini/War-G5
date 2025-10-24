0dafc83205e6ac63e652da029782946b837ed152
Rafael <139813923+Rafael-Machado-27@users.noreply.github.com>
2025-10-24 16:58:36 -0300

    Incrementando teste base do tabuleiro

--- back/model/teste_tabuleiro.py
+++ back/model/teste_tabuleiro.py
@@ -3,23 +3,17 @@ from .Partida import Partida
 partida = Partida(3, 0, 120, [["Alice", "vermelho", "humano"], ["Bob", "azul", "humano"], ["Charlie", "verde", "humano"]])
 print("Ordem de turno:")
 print(partida.jogadores)
-print()
 
 partida.jogadores[0].territorios[0].exercitos = 3
 partida.tabuleiro.territorios[5].exercitos = 5
 
 for i in partida.jogadores:
     print(f"\nTerritórios do Jogador {i.cor}:")
-    print()
     for j in i.territorios:
-        print(j)
-        print(j.fronteiras)
-        print()
+        print(f"\n{j} - Fronteiras: {j.fronteiras}\n")
 
-print("\n\nRegiões com bônus:")
-for i in partida.tabuleiro.regioes_com_bonus:
-    print(i)
-    print()
+print("\nRegiões com bônus:")
+print(partida.tabuleiro.regioes_com_bonus)
 
 print(f"\n\nTerritórios em tabuleiro: {len(partida.tabuleiro.territorios)}")
 
@@ -31,3 +25,10 @@ print()
 for i in partida.jogadores:
     partida.tabuleiro.calcula_exercitos_a_receber(i)
     print(f"O Jogador {i.cor} tem {i.exercitos_reserva} exercitos a receber nesse turno")
+
+for i in partida.tabuleiro.regioes_com_bonus[0][2]:
+    i.cor = partida.jogadores[0].cor
+
+print("\nDando territórios da Região 1 para o primeiro Jogador:")
+partida.tabuleiro.calcula_exercitos_a_receber(partida.jogadores[0])
+print(f"O Jogador {partida.jogadores[0].cor} tem {partida.jogadores[0].exercitos_reserva} exercitos a receber nesse turno")
